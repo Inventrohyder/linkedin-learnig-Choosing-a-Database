c7b4610cca4913f56878c7c644a44ad63c070c9d
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-04-07 11:00:57 +0300

    🧐 feat: using the INTERSECT function

--- 4_postgres_functions.sql
+++ 4_postgres_functions.sql
@@ -10,3 +10,18 @@ LIMIT 10;
 SELECT *
 FROM customer
 FETCH FIRST 10 ROWS ONLY;
+
+-- intersect statement
+-- this is similar to the INNER JOIN
+SELECT DISTINCT c.id
+FROM customer c
+JOIN "Order" o ON o.customerId = c.id;
+
+SELECT id
+FROM customer
+INTERSECT
+SELECT customerID
+FROM "Order";
+-- We don't require the DISTINCT keyword while using INTERSECT
+-- It is implied by INTERSECT and it chooses the values making
+-- them appear once
