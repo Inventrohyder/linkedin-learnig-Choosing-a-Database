3b74800ed5ce9b88a0c8446712f0d0f7b6485413
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-04-07 12:54:41 +0300

    🗃 feat: using JSON objects

--- 5_advanced_postgres.sql
+++ 5_advanced_postgres.sql
@@ -13,3 +13,22 @@ CREATE TABLE desserts
 -- check on the desserts table
 SELECT *
 FROM desserts;
+
+-- json objects
+-- Builds JSON objects and returns them
+SELECT JSON_BUILD_OBJECT('id', ProductID, 'name', ProductName, 'category', category)
+FROM product;
+
+-- json object into a table
+CREATE TABLE product_json
+(
+    product_names JSON
+);
+
+-- insert into the product json table
+INSERT INTO product_json
+SELECT JSON_BUILD_OBJECT('id', ProductID, 'name', ProductName, 'category', category)
+FROM product;
+
+SELECT *
+FROM product_json;
